3623b34b64e5c1621cde623ae596df805d6d7c43
Katherine Wren <kwren@outlook.com>
2019-03-26 18:47:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var recipes = new Object();
 function updateObjectWithKeyAndValue(object, key, value) {return Object.assign({},object,{[key]:value})}
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {object[key] = value; return object}
-function deleteFromObjectByKey(object, key) {return Object.assign({}, )
+function deleteFromObjectByKey(object, key) {return Object.assign({}, delete.key)
