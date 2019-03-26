307d55a21c00f190f13764ded58856d562939617
Katherine Wren <kwren@outlook.com>
2019-03-26 18:40:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 var recipes = new Object();
-function updateObjectWithKeyAndValue(object, key, value) {object[key] = value; return }
+function updateObjectWithKeyAndValue(object, key, value) {object[key] = value; return object}
